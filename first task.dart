aa473a76f82a8a574a6d057de223deea10ab34fb
DAVID <davidaddel0@gmail.com>
2025-04-24 22:41:33 +0200

    Add files via upload

---
+++ first task.dart
@@ -0,0 +1,11 @@
+//
+void main() {
+  //first task
+for (int x = 1; x <= 12; x++) {
+print("=========you are in $x===============");
+  for(int y =1; y<=12 ; y++){
+    print("$x * $y = ${x*y}");
+  }
+
+}
+}
